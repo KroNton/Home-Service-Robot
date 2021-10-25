0d31f9924d6197352219083b98ada4643556fc7f
KroNton <kyrlosfekry@gmail.com>
2021-10-25 15:17:40 +0200

    modify scrips to launch the world and map form any machine

--- pick_objects.sh
+++ pick_objects.sh
@@ -3,12 +3,13 @@ xterm  -e  " source /opt/ros/melodic/setup.bash; roscore " &
 
 sleep 5
 
-xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/kyrillos/catkin_ws/src/Home-Service-Robot/hs_robot/worlds/maze.world
+xterm  -e  "export ROBOT_INITIAL_POSE='-x -5 -y -2 -z 0 -R 0 -P 0 -Y 0';
+roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../../src/Home-Service-Robot/hs_robot/worlds/maze.world
  " & 
 
 sleep 5
 
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/kyrillos/catkin_ws/src/Home-Service-Robot/hs_robot/maps/mymap.yaml
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../../src/Home-Service-Robot/hs_robot/maps/mymap.yaml
 " & 
 
 sleep 5
