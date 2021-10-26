ea18e0edc14464945774574a1fbd61741067ced3
KroNton <kyrlosfekry@gmail.com>
2021-10-26 20:40:24 +0200

    creating  final marks node

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -11,6 +11,7 @@ int main( int argc, char** argv )
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
 
+   uint32_t marks_counter=0;
   while (ros::ok())
   {
     visualization_msgs::Marker marker;
@@ -22,23 +23,10 @@ int main( int argc, char** argv )
     // Any marker sent with the same namespace and id will overwrite the old one
     marker.ns = "basic_shapes";
     marker.id = 0;
-
+    marker.lifetime = ros::Duration();
     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
     marker.type = shape;
 
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::ADD;
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 3;
-    marker.pose.position.y = -1;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
     marker.scale.x = 1.0;
     marker.scale.y = 1.0;
     marker.scale.z = 1.0;
@@ -48,12 +36,39 @@ int main( int argc, char** argv )
     marker.color.g = 1.0f;
     marker.color.b = 0.0f;
     marker.color.a = 1.0;
-    marker.lifetime = ros::Duration(5.0);
-    marker_pub.publish(marker);
     
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
     
+
+  if(marks_counter==0){
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    ROS_INFO("adding pickup marker");
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.position.x = 3;
+    marker.pose.position.y = -1;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+    sleep(5.0);
+    marks_counter++;
     
-   /* marker.action = visualization_msgs::Marker::ADD;
+  }
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  else if(marks_counter==1){
+    sleep(5.0);
+    ROS_INFO("removing pickup marker");
+    marker.action = visualization_msgs::Marker::DELETE;
+    marks_counter++;
+  }
+
+    else if(marks_counter==2){
+      ROS_INFO("watting the robot to reach the goal");
+      sleep(5.0);
+      ROS_INFO("adding drop off marker");
+    marker.action = visualization_msgs::Marker::ADD;
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
     marker.pose.position.x = 2;
     marker.pose.position.y = 6;
@@ -62,20 +77,22 @@ int main( int argc, char** argv )
     marker.pose.orientation.y = 0.0;
     marker.pose.orientation.z = 0.0;
     marker.pose.orientation.w = 1.0;
-    marker.lifetime = ros::Duration(5.0);
-    marker_pub.publish(marker); */
+  
+    }
+  
+    marker_pub.publish(marker); 
 
     
         // Publish the marker
-   /* while (marker_pub.getNumSubscribers() < 1)
+    while (marker_pub.getNumSubscribers() < 1)
     {
       if (!ros::ok())
       {
         return 0;
-      }
+      
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
-    }*/
+    }
 
     // Cycle between different shapes
     /* switch (shape)
