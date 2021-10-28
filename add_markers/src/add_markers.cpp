8abcd631bbf8d631a6e1df2ef69aa5c1ab38f59f
KroNton <kyrlosfekry@gmail.com>
2021-10-28 22:36:25 +0200

    changing topic and msg

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <nav_msgs/Odometry.h>
 #include <visualization_msgs/Marker.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 double pickup_x_pose =3.0;
 double pickup_y_pose = -1.0;
@@ -11,17 +12,22 @@ double dropoff_y_pose = -2.0;
 bool item_pickedup = false;
 bool item_droppedoff = false;
 
-void robot_pose_Callback(const nav_msgs::Odometry::ConstPtr& poses) {
+void robot_pose_Callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& poses) {
   double robot_x = poses->pose.pose.position.x;
   double robot_y = poses->pose.pose.position.y;
 double pickup_distance = sqrt(pow(robot_x - pickup_x_pose, 2) + pow(robot_y - pickup_y_pose, 2));
- if (pickup_distance < 0.6) {
+
+
+
+ if (pickup_distance < 0.3) {
     item_pickedup = true;
+    ROS_INFO("your Robot Reached the pickup zone!!!");
   }
 
 double drop_off_distance = sqrt(pow(robot_x - dropoff_x_pose, 2) + pow(robot_y - dropoff_y_pose, 2));
-    if (drop_off_distance < 0.6) {
+    if (drop_off_distance < 0.3) {
     item_droppedoff = true;
+    ROS_INFO("your Robot Reached the dropoff zone!!!");
   } 
 
 }
@@ -46,13 +52,13 @@ double drop_off_distance = sqrt(pow(robot_x - dropoff_x_pose, 2) + pow(robot_y -
 int main( int argc, char** argv )
 {
 
-  ros::init(argc, argv, "basic_shapes");
+  ros::init(argc, argv, "goal_marker");
   ros::NodeHandle n;
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-   ros::Subscriber odom_sub = n.subscribe("odom", 10, robot_pose_Callback);
-  // Set our initial shape type to be a cube
-  uint32_t shape = visualization_msgs::Marker::CUBE;
+   ros::Subscriber odom_sub = n.subscribe("/amcl_pose", 10,robot_pose_Callback);
+  
+  uint32_t shape = visualization_msgs::Marker::SPHERE;
 
    
   
@@ -74,9 +80,9 @@ int main( int argc, char** argv )
     marker.pose.orientation.y = 0.0;
     marker.pose.orientation.z = 0.0;
     marker.pose.orientation.w = 1.0;
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
+    marker.scale.x = .50;
+    marker.scale.y = .50;
+    marker.scale.z = .50;
 
     // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
@@ -97,29 +103,28 @@ int main( int argc, char** argv )
       
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
-    }
+    }}
 
-  if(item_pickedup == false && item_droppedoff == false){
+  if(item_pickedup == false ){
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    ROS_INFO("adding pickup marker");
+   // ROS_INFO("adding pickup marker");
     marker.action = visualization_msgs::Marker::ADD;
     marker.pose.position.x = pickup_x_pose;
     marker.pose.position.y = pickup_y_pose;
-
+    
     marker_pub.publish(marker);
   }
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
-  else if(item_pickedup== true && item_droppedoff == false){
-    sleep(5.0);
-    ROS_INFO("removing pickup marker");
+  else if( item_droppedoff == false){
+    
+    //ROS_INFO("removing pickup marker");
     marker.action = visualization_msgs::Marker::DELETE;
     marker_pub.publish(marker); 
+    sleep(5.0);
   }
-
+    
      else if(item_droppedoff==true){
-      sleep(2.0);
-      
-      ROS_INFO("adding drop off marker");
+     // ROS_INFO("adding drop off marker");
     marker.action = visualization_msgs::Marker::ADD;
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
     marker.pose.position.x = dropoff_x_pose;
@@ -134,7 +139,7 @@ int main( int argc, char** argv )
         // Publish the marker
     
 
-    r.sleep();
-  }
+    ros::spinOnce();
+  
 }
 }
