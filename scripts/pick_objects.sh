67dd9764266b149ce9464f6ea4f9ae8618f043b9
KroNton <kyrlosfekry@gmail.com>
2021-10-29 16:38:46 +0200

    sourcing the workspace

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -3,19 +3,23 @@ xterm  -e  " source /opt/ros/melodic/setup.bash; roscore " &
 
 sleep 5
 
-xterm  -e  "export ROBOT_INITIAL_POSE='-x -5 -y -2 -z 0 -R 0 -P 0 -Y 0';
+xterm  -e  "source devel/setup.bash;
+export ROBOT_INITIAL_POSE='-x -5 -y -2 -z 0 -R 0 -P 0 -Y 0';
 roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../../src/Home-Service-Robot/hs_robot/worlds/maze.world
  " & 
 
 sleep 5
 
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../../src/Home-Service-Robot/hs_robot/maps/mymap.yaml
+xterm  -e  "source devel/setup.bash;
+roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../../src/Home-Service-Robot/hs_robot/maps/mymap.yaml
 " & 
 
 sleep 5
 
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " & 
+xterm  -e  "source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch " & 
 
 sleep 5
 
-xterm  -e  "rosrun pick_objects pickup_objects "
+xterm  -e  "source devel/setup.bash;
+rosrun pick_objects pickup_objects "
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -3,19 +3,23 @@ xterm  -e  " source /opt/ros/melodic/setup.bash; roscore " &
 
 sleep 5
 
-xterm  -e  "export ROBOT_INITIAL_POSE='-x -5 -y -2 -z 0 -R 0 -P 0 -Y 0';
+xterm  -e  "source devel/setup.bash;
+export ROBOT_INITIAL_POSE='-x -5 -y -2 -z 0 -R 0 -P 0 -Y 0';
 roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../../src/Home-Service-Robot/hs_robot/worlds/maze.world
  " & 
 
 sleep 5
 
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " & 
+xterm  -e  "source devel/setup.bash;
+ roslaunch turtlebot_gazebo gmapping_demo.launch " & 
 
 sleep 5
 
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " & 
+xterm  -e  "source devel/setup.bash;
+ roslaunch turtlebot_rviz_launchers view_navigation.launch " & 
 
 sleep 5
 
-xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch 
+xterm  -e  "source devel/setup.bash;
+roslaunch turtlebot_teleop keyboard_teleop.launch 
  " 
