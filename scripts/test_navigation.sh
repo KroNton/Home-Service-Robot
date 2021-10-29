8b14958173805863ad5cb5cfea40c03f569f6738
KroNton <kyrlosfekry@gmail.com>
2021-10-29 18:27:21 +0200

    moving to the right directory before sourcing

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -3,18 +3,21 @@ xterm  -e  " source /opt/ros/melodic/setup.bash; roscore " &
 
 sleep 5
 
-xterm  -e  "source devel/setup.bash;
+xterm  -e  "cd $(pwd)/../..;
+source devel/setup.bash;
 export ROBOT_INITIAL_POSE='-x -5 -y -2 -z 0 -R 0 -P 0 -Y 0';
-roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../../src/Home-Service-Robot/hs_robot/worlds/maze.world
+roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/kyrillos/catkin_ws/src/Home-Service-Robot/hs_robot/worlds/maze.world
  " & 
 
 sleep 5
 
-xterm  -e  "source devel/setup.bash;
+xterm  -e  "cd $(pwd)/../..;
+source devel/setup.bash;
 roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../../src/Home-Service-Robot/hs_robot/maps/mymap.yaml
 " & 
 
 sleep 5
 
-xterm  -e  "source devel/setup.bash;
+xterm  -e  "cd $(pwd)/../..;
+source devel/setup.bash;
 roslaunch turtlebot_rviz_launchers view_navigation.launch " 
